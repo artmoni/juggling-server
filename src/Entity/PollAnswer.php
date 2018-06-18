3eac0208e8e1b7d0d5cd7cbc409ec2fe7c2dad49
sheshire <laurie.fernanli@gmail.com>
2018-06-18 15:04:26 +0200

    Creation of PollAnswer

---
+++ src/Entity/PollAnswer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PollAnswerRepository")
+ */
+class PollAnswer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $answer;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+}
