e21d8d3d2957ed9f306d0d7f5456a6e8d5fc857a
sheshire <laurie.fernanli@gmail.com>
2018-06-25 17:04:44 +0200

    Display the first poll who doesn't respond user

--- src/Controller/SurveyPollController.php
+++ src/Controller/SurveyPollController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\SurveyAnswer;
 use App\Entity\SurveyPoll;
 use App\Entity\User;
-use App\Repository\SurveyPollRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,7 +29,7 @@ class SurveyPollController extends Controller
     }
 
     /**
-     * @Route("/surveys/polls/{id}", name="survey_polls_user")
+     * @Route("/surveys/polls/users/{id}", name="survey_polls_user")
      * @Method("GET")
      * @param $id
      * @return JsonResponse
@@ -42,47 +41,27 @@ class SurveyPollController extends Controller
         $user = $entityManager->getRepository(User::class)->find($id);
 
         $surveyToSend = null;
-        $surveys = $entityManager->getRepository(SurveyPoll::class)->findAll();
-        $answers = $entityManager->getRepository(SurveyAnswer::class)->findBy(['user' => $user]);
         // dump($answers);
-        foreach ($surveys as $survey) {
-//            dump($survey);
-            if ($survey instanceof SurveyPoll) {
-                foreach ($answers as $answer) {
-                    if ($answer instanceof SurveyAnswer) {
-                        // dump($answer);
-                        if ($survey->getId() == $answer->getSurveyPoll()->getId()) {
-                            continue;
-                        } else {
-                            $surveyToSend = $survey;
-                            dump($surveyToSend);
-                            break;
-                        }
-                    }
-                    break;
-                }
-            } else
-            echo " Not survey";
-
-        }
+        $surveys = $entityManager->getRepository(SurveyPoll::class)->findAllSurveys($user);
+        $surveyToSend = $surveys[0];
         $surveys_json = $this->get('jms_serializer')->serialize($surveyToSend, 'json');
         return new JsonResponse(json_decode($surveys_json));
 
     }
 
-//    /**
-//     * @Route("/surveys/polls/{id}", name="survey_polls_id")
-//     * @Method("GET")
-//     */
-//    public
-//    function show($id)
-//    {
-//        $entityManager = $this->getDoctrine()->getManager();
-//        $surveys = $entityManager->getRepository(SurveyPoll::class)->find($id);
-//        $surveys_json = $this->get('jms_serializer')->serialize($surveys, 'json');
-//        return new JsonResponse(json_decode($surveys_json));
-//
-//    }
+    /**
+     * @Route("/surveys/polls/{id}", name="survey_polls_id")
+     * @Method("GET")
+     */
+    public
+    function show($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $surveys = $entityManager->getRepository(SurveyPoll::class)->find($id);
+        $surveys_json = $this->get('jms_serializer')->serialize($surveys, 'json');
+        return new JsonResponse(json_decode($surveys_json));
+
+    }
 
     /**
      * @Route("/surveys/polls" , name="survey_poll_create")
