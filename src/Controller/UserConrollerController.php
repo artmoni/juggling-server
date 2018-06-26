b34fe248c762905625ce7d78db30ee4578d30139
sheshire <laurie.fernanli@gmail.com>
2018-06-26 12:19:41 +0200

    WIP Update User profil

--- src/Controller/UserConrollerController.php
+++ src/Controller/UserConrollerController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,22 +23,37 @@ class UserConrollerController extends Controller
         return new JsonResponse();
     }
 
+//    /**
+//     * @Route("/users/{name}", name = "user_connect")
+//     * @Method("GET")
+//     */
+//    public function connexion($name){
+//        $entityManager = $this->getDoctrine()->getManager();
+//        $users = $entityManager->getRepository(User::class)->findBy(["name" => $name]);
+//        $user = $this->get('jms_serializer')->serialize($users, 'json');
+//        return new JsonResponse(json_decode($user));
+//
+//    }
+
     /**
-     * @Route("/users/{name}", name = "user_connect")
+     * @Route("/users/{id}", name = "user_id")
      * @Method("GET")
      */
-    public function connexion($name){
+    public function findUser($id)
+    {
         $entityManager = $this->getDoctrine()->getManager();
-        $users = $entityManager->getRepository(User::class)->findBy(["name" => $name]);
+        $users = $entityManager->getRepository(User::class)->find($id);
         $user = $this->get('jms_serializer')->serialize($users, 'json');
         return new JsonResponse(json_decode($user));
 
     }
+
     /**
      * @Route("/users", name = "user_subscribe")
      * @Method("POST")
      */
-    public function subscribe(Request $request){
+    public function subscribe(Request $request)
+    {
 
 //        $user = json_decode($request->getContent(), true);
 //
@@ -47,12 +63,43 @@ class UserConrollerController extends Controller
         $user = new User();
 //        $users = $entityManager->getRepository(User::class)->findOneBy($name);
 //        if($users == null && $user instanceof User) {
-            $entityManager->persist($user);
-            $entityManager->flush();
+        $entityManager->persist($user);
+        $entityManager->flush();
 //            return new JsonResponse();
 //        }
         return new JsonResponse(json_decode($this->get('jms_serializer')->serialize($user, 'json')));
     }
 
+    /**
+     * @Route("/users/update", name = "user_update")
+     * @Method("POST")
+     */
+    public function update(Request $request)
+    {
+
+        $userRequest = json_decode($request->getContent(), true);
+        $user = new User();
+        $user->setId($userRequest['id']);
+        $user->setName($userRequest['name']);
+        $user->setMail($userRequest['mail']);
+        $user->setTel($userRequest['tel']);
+
+        dump($user);
+
+        if (!$user instanceof User) throw new Exception("Not a User");
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $userToUpdate = $entityManager->getRepository(User::class)->find($user->getId());
+
+        $userToUpdate->setName($user->getName());
+        $userToUpdate->setMail($user->getMail());
+        $userToUpdate->setTel($user->getTel());
+
+        $entityManager->persist($userToUpdate);
+        $entityManager->flush();
+//            return new JsonResponse();
+//        }
+        return new JsonResponse(json_decode($this->get('jms_serializer')->serialize($userToUpdate, 'json')));
+    }
 
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -50,6 +50,13 @@ class User
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getMail(): ?string
     {
         return $this->mail;
